b040043d1f90a65b55589f15de57b31819edccdf
manhleodz <doanducmanh100@gmail.com>
2024-05-08 18:27:39 +0700

    updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken =
   "pk.eyJ1IjoiaGF1c3VyYSIsImEiOiJjbHZ1cmNvZW8xbzZ2MmptZzc1aWhmOHcwIn0.cdeEUNXMyKRO73ILEEuUsQ";
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/outdoors-v12", // style URL
+  style: 'mapbox://styles/mapbox/satellite-v9',
   center: [106.9230, 16.84769],
   zoom: 3, // starting zoom
   hash: 'map',
@@ -26,21 +26,28 @@ function addDataBoundary() {
     type: 'line',
     source: 'country-boundary',
     paint: {
-      'line-color': 'black',
+      'line-color': 'yellow',
       'line-width': 1
     }
   });
+
 }
 
 const popup = new mapboxgl.Popup({ offset: 25 }).setText(
 
 )
+
+map.on('style.load', () => {
+  map.setConfigProperty('basemap', 'lightPreset', 'dusk');
+});
+
 map.on("load", (e) => {
+
   addDataBoundary();
   map.flyTo({
     center: [106.85508306716277, 16.722048835190567],
     zoom: 5, // starting zoom
-  })
+  });
 
 });
 
@@ -54,54 +61,6 @@ function evtClickMap(e) {
 map.on("click", listener["evtClickMap"]);
 map.on("moveend", (e) => {
   console.log(e);
-});
-
-// const backToVn = document.getElementById("back-to-vn");
-// const zoomHn = document.getElementById("zoom-hn");
-
-// Xử lý sự kiện khi click Vietnam
-document.getElementById("back-to-vn").addEventListener('click', function () {
-  map.flyTo({
-    center: [106.9230, 16.84769],
-    zoom: 5, // starting zoom
-  });
-
-  const boundaryLayerName = "hanoi-boundary-layer";
-  const boundarySourceName = "hanoi-boundary";
-
-  if (map.getLayer(boundaryLayerName)) {
-    map.removeLayer(boundaryLayerName);
-  }
-  if (map.getSource(boundarySourceName)) {
-    map.removeSource(boundarySourceName);
-  }
-});
-
-// Xử lý sự kiện khi click Ha Noi
-document.getElementById("zoom-hn").addEventListener('click', function () {
-
-  const HaNoiBoundary = './boundaries/hanoi.geojson'
-  map.addSource('hanoi-boundary', {
-    type: 'geojson',
-    data: HaNoiBoundary
-  });
-
-  // Thêm lớp ranh giới
-  map.addLayer({
-    id: 'hanoi-boundary-layer',
-    type: 'line',
-    source: 'hanoi-boundary',
-    paint: {
-      'line-color': 'red',
-      'line-width': 2
-    }
-  });
-
-  var franceCoordinates = [105.83438877958353, 21.023701008522863];
-  map.flyTo({
-    center: franceCoordinates,
-    zoom: 12
-  });
 });
 
 //Hide box chat
@@ -137,12 +96,22 @@ function sendMessage() {
   inputField.value = "";
 }
 
+// Tạo một danh sách để lưu trữ các marker và popup
+var markers = [];
+var popups = [];
 //reload box chat
 function resetMessageList() {
   var messageList = document.getElementById('list_message');
   messageList.innerHTML = '';
-}
+  // Xóa tất cả các popup và marker
+  markers.forEach(function (marker) {
+    marker.remove();
+  })
 
+  popups.forEach(function (popup) {
+    popup.remove();
+  })
+}
 
 // buttom zoom map
 const navigationCtr = new mapboxgl.NavigationControl({
@@ -164,6 +133,7 @@ const geoCtr = new mapboxgl.GeolocateControl({
 });
 
 map.addControl(geoCtr, "top-right");
+map.addControl(new mapboxgl.FullscreenControl());
 
 //Search address
 function geocodeAddress(address) {
@@ -183,7 +153,47 @@ function geocodeAddress(address) {
         zoom: 12
       });
 
-      voiceControl(result.place_name);
+      console.log(result);
+
+      // Tạo marker
+      var marker = new mapboxgl.Marker()
+        .setLngLat(coordinates)
+        .addTo(map);
+
+      const markerHeight = 50;
+      const markerRadius = 10;
+      const linearOffset = 25;
+      const popupOffsets = {
+        'top': [0, 0],
+        'top-left': [0, 0],
+        'top-right': [0, 0],
+        'bottom': [0, -markerHeight],
+        'bottom-left': [linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+        'bottom-right': [-linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+        'left': [markerRadius, (markerHeight - markerRadius) * -1],
+        'right': [-markerRadius, (markerHeight - markerRadius) * -1]
+      };
+      const popup = new mapboxgl.Popup({ offset: popupOffsets, className: 'my-class' })
+        .setLngLat(coordinates)
+        .setHTML(`
+            <h1 class="text-lg">${result.place_name}</h1>
+        `)
+        .setMaxWidth("300px")
+        .addTo(map);
+      popups.push(popup);
+
+      // Thiết lập popup cho marker
+      marker.setPopup(popup);
+
+      // Mở popup khi marker được nhấp
+      marker.getElement().addEventListener('click', function () {
+        marker.togglePopup();
+      });
+
+      markers.push(marker);
+      if (localStorage.getItem("voice") && localStorage.getItem("voice") == "true") {
+        voiceControl(result.place_name);
+      }
 
       var divItem = document.createElement('div');
       divItem.className = 'w-full';
@@ -206,51 +216,34 @@ function geocodeAddress(address) {
     });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+let form_chat = document.getElementById("form_chat");
+form_chat.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // document.addEventListener("DOMContentLoaded", () => {
   const inputField = document.getElementById("input");
 
-  inputField.addEventListener("keydown", function (e) {
-    if (e.code === "Enter") {
-      let input = inputField.value.trim();
+  // inputField.addEventListener("keydown", function (e) {
+  //   if (e.code === "Enter") {
+  let input = inputField.value.trim();
 
-      if (input != "") {
-        var divItem = document.createElement('div');
-        divItem.className = 'w-full';
-        divItem.innerHTML = `
-          <div class="flex items-center justify-end w-full mb-1 space-x-2">
-            <h1 class="p-2 rounded-2xl text-white bg-blue-500 text-[14.5px] break-words" style="max-width: 230px;">${input}</h1>
-          </div>
-        `;
-        document.getElementById('list_message').appendChild(divItem);
-        var messageContainer = document.getElementById('list_message');
-        messageContainer.scrollTop = messageContainer.scrollHeight;
-        geocodeAddress(input);
-      }
-      inputField.value = "";
-    }
-  });
-
-  document.getElementById("btn_sendMessage").addEventListener("click", function () {
-    let input = inputField.value.trim();
-
-    if (input != "") {
-      var divItem = document.createElement('div');
-      divItem.className = 'w-full';
-      divItem.innerHTML = `
-          <div class="flex items-center justify-end w-full mb-1 space-x-2">
-            <h1 class="p-2 rounded-2xl text-white bg-blue-500 text-[14.5px] break-words" style="max-width: 230px;">${input}</h1>
-          </div>
-        `;
-      document.getElementById('list_message').appendChild(divItem);
-      var messageContainer = document.getElementById('list_message');
-      messageContainer.scrollTop = messageContainer.scrollHeight;
-      geocodeAddress(input);
-    }
-    inputField.value = "";
-  });
-
+  if (input != "") {
+    var divItem = document.createElement('div');
+    divItem.className = 'w-full';
+    divItem.innerHTML = `
+            <div class="flex items-center justify-end w-full mb-1 space-x-2">
+              <h1 class="p-2 rounded-2xl text-white bg-blue-500 text-[14.5px] break-words" style="max-width: 230px;">${input}</h1>
+            </div>
+          `;
+    document.getElementById('list_message').appendChild(divItem);
+    var messageContainer = document.getElementById('list_message');
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+    geocodeAddress(input);
+  }
+  inputField.value = "";
 
   document.getElementById('reload').addEventListener('click', function () {
     resetMessageList();
   });
-});
+
+})
